fd9f51257bf8ada16b8c7cc812cf156ffcd0e340
BenZeta <benpardede.hacktiv8@gmail.com>
2024-12-05 17:57:49 +0700

    deploy v5

--- app.js
+++ app.js
@@ -10,7 +10,8 @@ const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(
   session({
