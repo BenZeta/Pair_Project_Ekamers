0920978229603525940421515e9ccf606487612e
BenZeta <benpardede.hacktiv8@gmail.com>
2024-12-05 17:28:48 +0700

    fix:try fixing deploy 1

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-require("dotenv").config();
-
-if (process.env.NODE_ENV !== 'production') {
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
 
 const express = require("express");
 const router = require("./routers");
 const session = require("express-session");
+const RedisStore = require("connect-redis")(session);
+const redis = require("redis");
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 
+const client = redis.createClient({
+  host: "localhost", // Replace with your Redis server info
+  port: 6379, // Default Redis port
+});
+
 app.use(
   session({
+    store: new RedisStore({ client }),
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
